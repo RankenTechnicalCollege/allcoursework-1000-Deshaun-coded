2f47620a09dd407bd4fc326aa4bc4b0a926d5822
Vineyard_Coded <deshaunbrown933@yahoo.com>
2024-10-09 12:30:18 -0500

    Commit Summary

---
+++ labs/JS_CH02_LAB/Javascript/fahrenheit_to_celsius.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//initialize total variable
+let f=32
+
+//get 3 scores from user and add them together
+const fahrenheit= parseInt(prompt("Enter Fahrenheit temperature"));
+const celsius= (32)
+
+
+const result= parseFloat(prompt(total/3));
+
+const html= `<p>Score 1 =${Fahrenheit}</p>
+             <p>Average score= ${average.toFixed(1) }</p>`;
+        document.write(html);
--- labs/JS_CH02_LAB/Javascript/mpg.js
+++ labs/JS_CH02_LAB/Javascript/mpg.js
@@ -1,15 +1,15 @@
 "use strict";
 
     // get miles driven from user
-    const miles= parseInt (prompt("Enter miles driven"));
+    const miles= parseFloat (prompt("Enter miles driven"));
 
     //get gallons used from user
-    const gallons= parseInt(prompt("Enter gallons of gas used"));
+    const gallons= parseFloat(prompt("Enter gallons of gas used"));
 
     //calculate mpg
     const mpg= parseFloat (prompt(miles/gallons));
 
-       const  html=`<p>Miles: ${mile}</p>
-                <p>Gallons: ${gallons}</p>
-                <p>MPG: ${mpg.toFixed(2) }</p>`;
+       const  html=`<p><label>Miles: ${mile.toFixed(2)}</p>
+                <p><label>Gallons: ${gallons.toFixed(2)}</p>
+                <p><label>MPG: ${mpg.toFixed(2) }</p>`;
             document.write(html);
