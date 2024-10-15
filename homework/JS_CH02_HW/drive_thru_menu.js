3444eed34f8db97788995db9af80a99ec908985b
Vineyard_Coded <deshaunbrown933@yahoo.com>
2024-10-15 15:01:33 -0500

    Commit Summary

--- homework/JS_CH02_HW/drive_thru_menu.js
+++ homework/JS_CH02_HW/drive_thru_menu.js
@@ -1,22 +1,31 @@
-
-
-let total=0;
+let total = 0;
 
 //get 3 scores from user and add them together
-const burgers= parseFloat(prompt("Enter test scores"));
+const burgers = (("Enter test scores"));
 total += burgers;
 
-const fries=parseFloat(prompt("Enter test score"));
-total+= fries;
+const fries = (("Enter test score"));
+total += fries;
 
-const sodas= parseFloat(prompt("Enter test Score"));
-total+= sodas;
+const sodas = (("Enter test Score"));
+total += sodas;
 
-const average= parseFloat(total+=3);
+const average = (total += 3);
 
-const html= `<p>Score 1 =${burger}</p>
-             <p>Score 2 =${fries}</p>
+const html = 
+             `<p>Score 2 =${fries}</p>
              <p>Score 3 =${sodas}</p>
              <p>Average score= ${average.toFixed(2) }</p>`;
-        document.getElementById(submit).onclick =() => {
-        };
+
+
+let result = ;
+
+document.getElementById('btnClick').onclick = () => {
+
+};
+
+
+
+//return type
+
+result.innerHTML = "pink"
---
+++ labs/JS_CH03_LAB/ch3-labP2.js
@@ -0,0 +1,23 @@
+const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+    
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
