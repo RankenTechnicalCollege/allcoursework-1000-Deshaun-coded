21fb4bcd494a06a5858c2991c339be0550fd33d6
Deshaun Brown <deshaunbrown933@yahoo.com>
2024-11-13 06:52:50 -0600

    commit

--- labs/JS_CH06_LAB/register/register.js
+++ labs/JS_CH06_LAB/register/register.js
@@ -42,19 +42,7 @@ const processEntries = () => {
       }
 };
 
-const p = document.createElement("p");
 
-const ptext = document.createTextNode("Total tax: 4.99");
-
-p.appendChild(ptext);
-
- 
-
-const main = $("main");
-
-main.appendChild(p);
-
-$("#message").text = "Hello!";
 
 const resetForm= () => {
       $("form").reset();
