8c76f0313ab7ad6e533924577ad7e4c96c57c1fb
Deshaun Brown <deshaunbrown933@yahoo.com>
2024-11-16 12:57:29 -0600

    Update future_value.js

--- labs/JS_CH04_LAB/future_value/future_value.js
+++ labs/JS_CH04_LAB/future_value/future_value.js
@@ -21,6 +21,35 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
   $("#calculate").addEventListener("click", (evt) => {
     // clear any previous calculation
-    $("#future_value").value
-  })
+    $("#future_value").value="";
+
+
+    //get values user entered in textboxes
+    const investment = parseFloat($("#investment").value);
+    const rate = parseFloat($("#rate").value);
+    const years = parseInt($("#years").value);
+
+    let errorMsg = "";
+
+    //check user entries for errors or invalid inputs
+    if (isInvalidValue(investment)){
+      errorMsg+= "Interest rate must be a valid number greater than zero.\n"
+      $("#investment").focus();
+    }
+
+    if (isInvalidValue(rate)){
+      errorMsg += "Interest rate must be a valid number greater than zero.\n";
+      $("#rate").focus();
+    }
+
+    if (isInvalidValue(years)){
+      errorMsg += "Number of years must be a valid number greater than zero.";
+      $("#years").focus();
+    } else{
+      //display error message
+      alert(errorMsg);
+    }
+    // set focus on first text box on initial load
+    $("#investment").focus();
+  });
 })
