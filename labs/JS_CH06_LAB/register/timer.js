0391d60cbc68f18cdbb740bc73c31c3e0fe631f2
Deshaun Brown <deshaunbrown933@yahoo.com>
2024-11-20 03:43:28 -0600

    commit summary

---
+++ labs/JS_CH06_LAB/register/timer.js
@@ -0,0 +1,31 @@
+
+const $ = selector => document.querySelector(selector);
+
+
+
+let timer = null;
+
+ let count = 10;
+const updateCounter = () => {
+
+    $("#counter").firstChild.nodeValue = count;
+     if (count <= 0) {
+         clearInterval(timer);
+
+        $("#counter").firstChild.nodeValue = "Blastoff!";
+
+    } 
+
+    count--;
+
+};
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+   updateCounter();
+
+  timer = setInterval(updateCounter, 1000);
+
+});
