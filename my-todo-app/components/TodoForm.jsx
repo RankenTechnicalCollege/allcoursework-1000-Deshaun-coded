3ac9689b013992a211d3346f2a23b549b5708ca2
Vineyard_Coded <deshaunbrown933@yahoo.com>
2024-12-18 12:40:23 -0600

    commit summary

--- my-todo-app/components/TodoForm.jsx
+++ my-todo-app/components/TodoForm.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 const TodoForm = () => {
   const {input, setInput}= useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random()*10000),
+      text:input
+    })
+    setInput('');
+  };
+
+  const handleChange = e => {
+    setInput(e.target.value);
+
+  };
+
+  
+
   return (
   <div>
-    <form className='todo-form'>
+    <form className='todo-form' onSubmit={handleSubmit}>
       <input type='text'
       placeholder='Update your task'
       value={input}
       name='text'
-      className='todo-input-edit'/>
+      className='todo-input-edit'
+      onChange={handleChange}/>
       <button type='submit' className='todo-button edit'>Update</button>
       </form>
       </div>
